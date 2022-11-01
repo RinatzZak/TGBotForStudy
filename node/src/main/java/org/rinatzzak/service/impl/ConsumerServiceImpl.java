0baa8096a336f6397491cf546d7fc4bf45af4e36
RinatzZak <zakirov.zrz@gmail.com>
2022-11-01 18:00:52 +0300

    add methods in ConsumerServiceImpl.java

--- node/src/main/java/org/rinatzzak/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/org/rinatzzak/service/impl/ConsumerServiceImpl.java
@@ -2,24 +2,44 @@ package org.rinatzzak.service.impl;
 
 import lombok.extern.log4j.Log4j;
 import org.rinatzzak.service.ConsumerService;
+import org.rinatzzak.service.ProduceService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static org.rinatzzak.model.RabbitQueue.*;
 
 @Service
 @Log4j
 public class ConsumerServiceImpl implements ConsumerService {
-    @Override
-    public void consumeTextMessageUpdates(Update update) {
-        log.debug("NODE: Text message is received");
+    private final ProduceService produceService;
+
+    public ConsumerServiceImpl(ProduceService produceService) {
+        this.produceService = produceService;
     }
 
     @Override
+    @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
+    public void consumeTextMessageUpdates(Update update) {
+        log.debug("NODE: Text message is received");
+
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("Hello from NODE");
+        produceService.produceAnswerMessage(sendMessage);
+    }
+
+    @Override
+    @RabbitListener(queues = DOC_MESSAGE_UPDATE)
     public void consumeDocumentMessageUpdates(Update update) {
         log.debug("NODE: Document message is received");
     }
 
     @Override
+    @RabbitListener(queues = PHOTO_MESSAGE_UPDATE)
     public void consumePhotoMessageUpdates(Update update) {
-        log.debug("NODE: Document message is received");
+        log.debug("NODE: Photo message is received");
     }
 }

338f5e4a1a3f0a14cdd4de28f9e511cd27aaaace
RinatzZak <zakirov.zrz@gmail.com>
2022-11-02 18:22:13 +0300

    add MainServiceImpl

---
+++ node/src/main/java/org/rinatzzak/service/impl/MainServiceImpl.java
@@ -0,0 +1,39 @@
+package org.rinatzzak.service.impl;
+
+import org.rinatzzak.dao.RawDataDao;
+import org.rinatzzak.entity.RawData;
+import org.rinatzzak.service.MainService;
+import org.rinatzzak.service.ProduceService;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class MainServiceImpl implements MainService {
+    private final RawDataDao rawDataDao;
+    private final ProduceService produceService;
+
+    public MainServiceImpl(RawDataDao rawDataDao, ProduceService produceService) {
+        this.rawDataDao = rawDataDao;
+        this.produceService = produceService;
+    }
+
+    @Override
+    public void processMessageText(Update update) {
+        saveRawData(update);
+
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("Hello from NODE");
+        produceService.produceAnswerMessage(sendMessage);
+    }
+
+    private void saveRawData(Update update) {
+        RawData rawData = RawData.builder()
+                .event(update)
+                .build();
+
+        rawDataDao.save(rawData);
+    }
+}

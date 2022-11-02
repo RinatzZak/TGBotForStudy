b461be742ad34758a5c263b7231683f966f9c2ee
RinatzZak <zakirov.zrz@gmail.com>
2022-11-02 17:35:21 +0300

    add dao for entity

---
+++ node/src/main/java/org/rinatzzak/dao/RawDataDao.java
@@ -0,0 +1,9 @@
+package org.rinatzzak.dao;
+
+import org.rinatzzak.entity.RawData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RawDataDao extends JpaRepository<RawData, Long> {
+
+}

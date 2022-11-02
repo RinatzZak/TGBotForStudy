d91ab4770bd6626e2749ce5eda9a2d475e15c035
RinatzZak <zakirov.zrz@gmail.com>
2022-11-02 17:45:11 +0300

    refactor

--- node/src/main/java/org/rinatzzak/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/org/rinatzzak/service/impl/ConsumerServiceImpl.java
@@ -2,10 +2,9 @@ package org.rinatzzak.service.impl;
 
 import lombok.extern.log4j.Log4j;
 import org.rinatzzak.service.ConsumerService;
-import org.rinatzzak.service.ProduceService;
+import org.rinatzzak.service.MainService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static org.rinatzzak.model.RabbitQueue.*;
@@ -13,22 +12,17 @@ import static org.rinatzzak.model.RabbitQueue.*;
 @Service
 @Log4j
 public class ConsumerServiceImpl implements ConsumerService {
-    private final ProduceService produceService;
+    private final MainService mainService;
 
-    public ConsumerServiceImpl(ProduceService produceService) {
-        this.produceService = produceService;
+    public ConsumerServiceImpl(MainService mainService) {
+        this.mainService = mainService;
     }
 
     @Override
     @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
     public void consumeTextMessageUpdates(Update update) {
         log.debug("NODE: Text message is received");
-
-        var message = update.getMessage();
-        var sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from NODE");
-        produceService.produceAnswerMessage(sendMessage);
+        mainService.processMessageText(update);
     }
 
     @Override

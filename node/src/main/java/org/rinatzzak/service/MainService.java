4c63843028343dacf5bcdd49e7154a58be65ab67
RinatzZak <zakirov.zrz@gmail.com>
2022-11-06 18:28:27 +0300

    add new methods for documents and photos

--- node/src/main/java/org/rinatzzak/service/MainService.java
+++ node/src/main/java/org/rinatzzak/service/MainService.java
@@ -4,4 +4,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 public interface MainService {
     void processMessageText(Update update);
+    void processDocumentText(Update update);
+    void processPhotoText(Update update);
 }
